481c8476d509f833d69e131a705b7763384ead08
Ilya Roschikhmarov <ilrosch@mail.ru>
2026-02-23 12:56:51 +0300

    connect db postgres + import models + other settings

--- shoes_shop/settings.py
+++ shoes_shop/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_bootstrap5',
+    'shoes',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'shoes_shop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -73,9 +75,13 @@ WSGI_APPLICATION = 'shoes_shop.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "demo_2026",
+        "USER": "postgres",
+        "PASSWORD": "qwerty",
+        "HOST": "127.0.0.1",
+        "PORT": "5433",
     }
 }
 
